668a8a82c8f3f54f1dfb56b53ebe5749bc2fec70
CastleBomber <richie.ucsb@gmail.com>
2021-05-20 14:28:57 -0700

    going to move into directories

--- src/sample/CRUDoptionsController.java
+++ src/sample/CRUDoptionsController.java
@@ -8,18 +8,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.control.PasswordField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-import java.io.File;
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.Statement;
 import java.util.ResourceBundle;
 
 public class CRUDoptionsController implements Initializable {
